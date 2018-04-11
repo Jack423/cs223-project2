8ee4e6c8ac074e62f97db183e2aca3d6ae7b5221
Jack Butler <guitar423@gmail.com>
2018-04-10 19:33:07 -0500

    Added project files.

---
+++ cmake-build-debug/CMakeFiles/cs223_project2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs223_project2.dir/main.cpp.o"
+  "cs223_project2.pdb"
+  "cs223_project2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs223_project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
