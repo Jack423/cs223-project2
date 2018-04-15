45e8ac0b7158fc1f6b5f46701da4881942c5756c
sCderb429 <ScottD8@cox.net>
2018-04-15 11:58:06 -0500

    Part 1: Updated
    testCorrectness Function Works as Expected
    
    testHashingTime Does Not Run(Don't Know Why)

---
+++ cmake-build-debug/CMakeFiles/part1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part1.dir/TestHashing.cpp.obj"
+  "part1.pdb"
+  "part1.exe"
+  "part1.exe.manifest"
+  "libpart1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/part2-3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part2-3.dir/TestHeap.cpp.obj"
+  "part2-3.pdb"
+  "part2-3.exe"
+  "part2-3.exe.manifest"
+  "libpart2-3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/part2-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
