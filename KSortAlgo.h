a0065a693a8cca971cf9dbd5af7e8c0d2d63e704
Jack Butler <guitar423@gmail.com>
2018-04-19 16:55:31 -0500

    Changed my code for part 3 but it still doesnt work

--- KSortAlgo.h
+++ KSortAlgo.h
@@ -11,15 +11,13 @@ class KSortAlgo {
 
 public:
     void sort(int kSortedArray[], int arrayLen, int k) { // fill this method
-        Heap *heap = new Heap(k + 1); //Create an empty heap with a size of k + 1
-        Heap *heap2 = new Heap(k + 1);
+        int *heapArray = new int[k + 1];
+        for (int i = 0; i <= k && i < arrayLen; i++)
+            heapArray[i] = kSortedArray[i];
+        Heap hp(k + 1);
 
-        for(int i = 0; i > arrayLen; i++) { //Add k+1 elements into the heap
-            heap->insert(kSortedArray[i]);
-        }
-
-        while(heap->getSize() > 0) {
-            heap2->insert(heap->deleteMinimum());
+        for (int i = k + 1, ti = 0; ti < arrayLen; i++, ti++) {
+            kSortedArray[ti] = hp.deleteMinimum();
         }
     }
 };
